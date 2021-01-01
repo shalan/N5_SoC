59f11b1fbfcfa38dd9439b328febf8a178a58e30
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-01-01 17:20:49 +0200

    Added power pins for DFFRAM

--- verilog/rtl/IPs/RAM_4Kx32.v
+++ verilog/rtl/IPs/RAM_4Kx32.v
@@ -42,6 +42,10 @@ module RAM_4Kx32 (
 	DFFRAM_4K
 `endif
             #(.COLS(4)) RAM (
+            `ifdef USE_POWER_PINS
+                .VPWR(VPWR),
+                .VGND(VGND),
+            `endif
                 .CLK(CLK),
                 .WE(WE),
                 .EN(_EN_[gi]),
